de9882d2174d45ee12ef156406626041a7c0add8
owenavon <89999663+owenavon@users.noreply.github.com>
2022-09-28 17:47:16 -0400

    Completed Exercise 1
    Exercise 1 - HTML5 Canvas API & File API

--- exercises/visual-composition/js/main.js
+++ exercises/visual-composition/js/main.js
@@ -1,6 +1,6 @@
 // Started with main,js & cart351_triangle.js code structure
 
-//empty array
+// Empty array
 let hourglassArray = [];
 let NUM_HG = 1;
 
@@ -10,18 +10,19 @@ window.onload = function() {
   canvas.width = window.innerWidth; // Returns the interior width of the window in pixels
   canvas.height = window.innerHeight; // Returns the interior height of the window in pixels
 
-  // OnLOAD...
-  for (let i=0; i<20; i++) {
-    hourglassArray.push (new HourglassShape(i,
-      (Math.random()*canvas.height),
-      (Math.random()*30)+100,context)
+  document.getElementById('inputText').addEventListener('change', readAndHandleTextFile); // Gets HTML content for file reader
+  document.getElementById("outer").style.visibility = "hidden"; // Starts with the file browser hidden
+
+  // Onload...
+  for (let i=0; i<1; i++) {
+    hourglassArray.push (new HourglassShape(i,(Math.random()*canvas.height),(Math.random()*30)+100,context) // 4 Items
     );
   }
 
   // Draws Introduction text on the Canvas
-  context.font = "100px Arial";
+  context.font = "100px calibri";
   context.textAlign = "center";
-  context.fillText("Click anywhere", canvas.width/2, canvas.height/2);
+  context.fillText("Click anywhere", canvas.width/2, canvas.height/2); // Positions text in centre
 
   // Allows the user to click anywhere on the canvas to start animation
   document.getElementById('canvasArea').addEventListener("click",mouseClickHandler);
@@ -35,19 +36,54 @@ window.onload = function() {
     }
   }
 
-  //ANYTHING OUTSIDE of animate function and not within another event handler WILL run only ONCE...
-
-  function animate() {
-  // Redraws the background between every animated movement
-  context.clearRect(0,0,canvas.width,canvas.height);
-
-  for (let i=0; i<NUM_HG; i++) {
-    hourglassArray[i].update(); // Triangle array current index
-    hourglassArray[i].checkBounds(canvas);
-    hourglassArray[i].display();
+  function fileBrowserVisible() { // Makes the file browser visible
+    document.getElementById("outer").style.visibility = "visible";
   }
-  requestAnimationFrame(animate); // JS function to tell browser to perform animation
 
-  } //end animate
+  function readAndHandleTextFile() { // get the file list ...
+  const selectedFileList = this.files;
+  const file = selectedFileList[0];
+    if (file.type.startsWith('text/')) {
+      const pTag = document.createElement("p"); // Create a new image element
+      const reader = new FileReader();
+      //once is read
+      reader.addEventListener("load", function () { // load = onRead
+        console.log(reader.result);
+        pTag.textContent = reader.result;
+        // append to the document
+        document.getElementById("textResult").appendChild(pTag);
+      });
+      // to read the file as text
+      reader.readAsText(file); // Returns a string
+    } // Ends if
+  } // Ends event handler
 
-} //end load
+  // Runs event handler infinity
+  function animate() {
+    context.clearRect(0,0,canvas.width,canvas.height); // Redraws the background between every animated movement
+
+    for (let i=0; i < hourglassArray.length; i++) {
+        if (hourglassArray[i].checkBounds(canvas) === true && hourglassArray[i].isActive === true) {
+          let hourglass = new HourglassShape(0,0,(Math.random()*30)+100,context); // 4 items. Starts hourglass from top left corner
+          hourglassArray.push(hourglass);
+
+          hourglassArray[i].isActive = false // Immediately return as false, so that a new instance is not created while the hourglass is touching the canvas bounds
+            setTimeout(() => {
+              console.log('Touch'); // Each time the hourglass touches the canvas
+              hourglassArray[i].isActive = true
+            }, 1000) // 1 second delay
+          } // close if
+
+          else if (hourglassArray.length === 150) { // Prevents the browser from crashing
+            fileBrowserVisible(); // Calls function to make File Browser Visisble
+            break; // Stops the loop
+          } // close else if
+
+          hourglassArray[i].update();
+          hourglassArray[i].display();
+        }  // close for loop
+
+        requestAnimationFrame(animate); // JS function to tell browser to perform animation
+    } // End animation
+
+  } // End load
--- exercises/visual-composition/js/shape.js
+++ exercises/visual-composition/js/shape.js
@@ -1,104 +1,92 @@
 // Started with main,js & cart351_triangle.js code structure
 
 class HourglassShape {
-    constructor(x,y,lineLength,context) {
+  constructor(x,y,lineLength,context) {
+    this.lineLength = lineLength; // member variable for the object (must prefix with .this)
+    this.localCanvasContext = context;
+    this.fillColor = "#ffffff";
+    this.strokeColor = "#f0f0f0";
+    this.isActive = true; // Process to create delay in shape generation
 
-      this.lineLength = lineLength; // member variable for the object (must prefix with .this)
-      this.localCanvasContext = context;
-      this.fillColor = "#ffffff";
-      this.strokeColor = "#f0f0f0";
-      this.anotherShape = 0;
+    // new vars for speed:
+    this.speedX  = 2;
+    this.speedY = 4;
 
-      //new vars for speed:
-      this.speedX  = 2;
-      this.speedY = 4;
+    // randomize speed
+    this.speedX  =(Math.random()*5)+2;
+    this.speedY  =(Math.random()*5)+2;;
 
-      //randomize speed
-      this.speedX  =(Math.random()*5)+2;
-      this.speedY  =(Math.random()*5)+2;;
-
-      //made into a method so that at any time we can update the triangle ...
-      this.setPoints(x,y);
-    }
-
-    randomColor() {
-      this.fillColor = `rgba(
-          ${Math.floor(Math.random()*255)},
-          ${Math.floor(Math.random()*255)},
-          ${Math.floor(Math.random()*255)},
-          ${Math.floor(Math.random()*255)}
-        )`;
-    }
-
-    // DOES NOT WORK
-    anotherShape() {
-      this.hourglassArray.length; i++
-    }
-
-    //method to display the triangle using the HTML 5 canvas API
-    display() {
-      this.localCanvasContext.fillStyle = this.fillColor; // Displays the color
-      this.localCanvasContext.addedShape = this.anotherShape; // Displays the new object
-
-      this.localCanvasContext.beginPath();
-      this.localCanvasContext.moveTo(this.x1,this.y1);
-
-      this.localCanvasContext.lineTo(this.x1,this.y1);
-      this.localCanvasContext.lineTo(this.x2,this.y2);
-      this.localCanvasContext.lineTo(this.x3,this.y3);
-      this.localCanvasContext.lineTo(this.x4,this.y4);
-
-      this.localCanvasContext.fill();
-
-      this.localCanvasContext.strokeStyle = this.strokeColor ;
-      this.localCanvasContext.lineWidth = 0; // Don't display lineWidth
-      this.localCanvasContext.stroke();
-      this.localCanvasContext.closePath();
-    }
-
-    //method to update the points ...
-    setPoints(x,y) {
-      //p1
-      this.x1 = x;
-      this.y1 = y;
-      //p2
-      this.x2 = this.x1+this.lineLength;
-      this.y2 = this.y1
-      //p3
-      this.x3 = this.x1
-      this.y3 = this.y1+this.lineLength;
-
-      this.x4 = this.x1+(this.lineLength);
-      this.y4 = this.y1+this.lineLength; // Creates hourglass symbol
-    }
-
-    update() {
-        let newX = this.x1+this.speedX;
-        let newY = this.y1+this.speedY;
-
-        //set the points
-        this.setPoints(newX,newY); // Helper function that is made in the constructor
-      }
-
-      checkBounds(localCanvas) {
-        if(this.x1>localCanvas.width || this.x1<0 ){
-          this.speedX = this.speedX*-1;
-          this.randomColor();
-          // this.anotherShape();
-
-
-
-
-
-
-        }
-
-        if(this.y1>localCanvas.height || this.y1<0 ){
-        this.speedY=this.speedY*-1;
-        this.randomColor();
-
-
-        }
-      }
+    // made into a method so that at any time we can update the triangle ...
+    this.setPoints(x,y);
   }
-  /** end class def **/
+
+  randomColor() {
+    this.fillColor = `rgba(
+      ${Math.floor(Math.random()*255)},
+      ${Math.floor(Math.random()*255)},
+      ${Math.floor(Math.random()*255)},
+      ${Math.floor(Math.random()*255)}
+    )`;
+  }
+
+  //method to display the triangle using the HTML 5 canvas API
+  display() {
+    this.localCanvasContext.fillStyle = this.fillColor; // Displays the color
+
+    this.localCanvasContext.beginPath();
+    this.localCanvasContext.moveTo(this.x1,this.y1);
+
+    this.localCanvasContext.lineTo(this.x1,this.y1);
+    this.localCanvasContext.lineTo(this.x2,this.y2);
+    this.localCanvasContext.lineTo(this.x3,this.y3);
+    this.localCanvasContext.lineTo(this.x4,this.y4);
+
+    this.localCanvasContext.fill();
+
+    this.localCanvasContext.strokeStyle = this.strokeColor ;
+    this.localCanvasContext.lineWidth = 0; // Don't display lineWidth
+    this.localCanvasContext.stroke();
+    this.localCanvasContext.closePath();
+  }
+
+  // Method to update the points
+  setPoints(x,y) {
+    //p1
+    this.x1 = x;
+    this.y1 = y;
+    //p2
+    this.x2 = this.x1+this.lineLength;
+    this.y2 = this.y1
+    //p3
+    this.x3 = this.x1
+    this.y3 = this.y1+this.lineLength;
+
+    this.x4 = this.x1+(this.lineLength);
+    this.y4 = this.y1+this.lineLength; // Creates hourglass symbol
+  }
+
+  update() {
+    let newX = this.x1+this.speedX;
+    let newY = this.y1+this.speedY;
+
+    // set the points
+    this.setPoints(newX,newY); // Helper function that is made in the constructor
+  }
+
+
+  checkBounds(localCanvas) {
+    if (this.x1>localCanvas.width || this.x1<0 ) {
+      this.speedX = this.speedX*-1;
+      this.randomColor();
+      return true;
+    }
+
+    if (this.y1>localCanvas.height || this.y1<0 ) {
+      this.speedY=this.speedY*-1;
+      this.randomColor();
+      return true;
+    }
+
+    return false;
+  } // CheckBounds
+} // Hourglass Shape
