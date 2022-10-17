d958bad554ee3caf54e64a5f3c928b38356de413
owenavon <89999663+owenavon@users.noreply.github.com>
2022-10-17 11:44:49 -0400

    Used for loop to display JSON objects
    Used for loop to display all JSON objects of the food [] on screen.

--- exercises/json-data/js/script.js
+++ exercises/json-data/js/script.js
@@ -2,21 +2,26 @@
 $(document).ready(function(){
   $("button").click(function(){
     $.getJSON("./loadFiles/weekendDinner.json", function(data){
-
-
+      
       let days = [];
-        $.each( data.food[0], function( key, val ) {
-        days.push( "<li id='" + key + "'>" + val + "</li>" );
-      });
 
       // Create for loop to run through each object
- 
+      for (let i = 0; i < data.food.length; i++) {
+        $.each( data.food[i], function( key, val ) {
+          days.push( "<li id='" + key + "'>" + val + "</li>" );
+        });
+      }
+      // console.log(data.food[i]);
+
       $( "<ul/>", {
         "class": "my-new-list",
         html: days.join( "" )
       }).appendTo( "body" );
+      // console.log(data.food);
 
-      console.log(data.food);
     });
   });
 });
+
+
+
