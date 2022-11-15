52f1882af68c862b03cf9050b0c1c2e2a1a8151e
owenavon <89999663+owenavon@users.noreply.github.com>
2022-11-15 17:33:55 -0500

    Exercise 3 - Progression
    Calendar input concept with appended riddles to create treasure hunt

---
+++ exercises/retrieve-post/actions/action-append-two.php
@@ -0,0 +1,13 @@
+<?php
+//If you use fopen() on a file that does not exist, it will create it,
+//given that the file is opened for writing (a)
+//nOTE  - "a" appends ....
+$theFile = fopen("../files/riddles.txt", "a") or die("Unable to open file!");
+$txt = "Sunday\n";
+fwrite($theFile, $txt);
+$txt2 = "Stromy\n";
+fwrite($theFile, $txt2);
+$txt3 = "Walked and walked  - got lodsgsgst \n";
+fwrite($theFile, $txt3);
+fclose($theFile);
+?>
---
+++ exercises/retrieve-post/js/empty.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    // $("#riddle1").click(function() {
+    //     $("#modalLogin").modal('show');
+    // })
+
+    
+})
